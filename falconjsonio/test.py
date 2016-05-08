5a9ff8e40440a78263bc7224ec172062101e20b5
Gary Monson <gary.monson@gmail.com>
2016-05-08 19:54:48 +1000

    Fix test names (duplicate names caused some tests not to run)

--- falconjsonio/test.py
+++ falconjsonio/test.py
@@ -242,37 +242,37 @@ On instance:
         self.assertEqual(json.loads(response.decode('utf-8')), {'title': 'Internal Server Error', 'description': 'Undisclosed'})
 
 ###
-    def test_no_accept(self):
+    def test_no_accept_inherited(self):
         response, = self.simulate_request('/good_child_response', method='POST', body=json.dumps({}), headers={})
         self.assertEqual(self.srmock.status, '415 Unsupported Media Type')
         self.assertEqual((json.loads(response.decode('utf-8')))['title'], 'Unsupported media type')
 
-    def test_unsupported_accept(self):
+    def test_unsupported_accept_inherited(self):
         response = self.simulate_request('/good_child_response', method='POST', body=json.dumps({}), headers={'Accept': 'text/html'})
         self.assertEqual(len(response), 0) # Unsupported media type, so we can't respond with a body
         self.assertEqual(self.srmock.status, '406 Not Acceptable')
 
-    def test_no_content_type(self):
+    def test_no_content_type_inherited(self):
         response = self.simulate_request('/good_child_response', method='POST', body=json.dumps({}), headers={'Accept': 'application/json'})
         self.assertEqual(self.srmock.status, '415 Unsupported Media Type')
 
-    def test_unsupported_content_type(self):
+    def test_unsupported_content_type_inherited(self):
         response = self.simulate_request('/good_child_response', method='POST', body=json.dumps({}), headers={'Accept': 'application/json', 'Content-Type': 'text/html'})
         self.assertEqual(self.srmock.status, '415 Unsupported Media Type')
 
-    def test_post(self):
+    def test_post_inherited(self):
         response = self.simulate_request('/good_child_response', method='POST', body=json.dumps({'email': 'foo@example.com', 'password': 'hunter2'}), headers={'Accept': 'application/json', 'Content-Type': 'application/json'})
         self.assertEqual(self.good_child_resource.received, {'email': 'foo@example.com', 'password': 'hunter2'})
         self.assertEqual(self.srmock.status, '201 Created')
         self.assertEqual(json.loads(response[0].decode('utf-8')), {'email': 'foo@example.com'})
 
-    def test_nonconforming_post_request(self):
+    def test_nonconforming_post_request_inherited(self):
         response = self.simulate_request('/good_child_response', method='POST', body=json.dumps({'email': 'foo@example.com'}), headers={'Accept': 'application/json', 'Content-Type': 'application/json'})
         self.assertEqual(self.good_child_resource.received, None)
         self.assertEqual(self.srmock.status, '400 Bad Request')
         self.assertEqual((json.loads(response[0].decode('utf-8')))['title'], 'Invalid request body')
 
-    def test_nonconforming_post_response(self):
+    def test_nonconforming_post_response_inherited(self):
         response, = self.simulate_request('/bad_child_response', method='POST', body=json.dumps({'email': 'foo@example.com', 'password': 'hunter2'}), headers={'Accept': 'application/json', 'Content-Type': 'application/json'})
         self.assertEqual(self.bad_child_resource.received, {'email': 'foo@example.com', 'password': 'hunter2'})
         self.assertEqual(self.srmock.status, '500 Internal Server Error')
